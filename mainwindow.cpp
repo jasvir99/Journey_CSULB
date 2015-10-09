f7ecc9d213f350a9774717c37f748a6557559f8a
jasvir99 <jassigrewal91@gmail.com>
2015-10-08 21:19:45 -0700

    adds combo box in ui. adds json file for holding information of rooms.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,11 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     QPixmap* image = new QPixmap(":/images/resources/board.png");
-    QPixmap* card = new QPixmap(":/images/resources/card1.png");
+    //QPixmap* card = new QPixmap(":/images/resources/card1.png"); pointer to cards
 
     ui->setupUi(this);
     ui->mainImage->setPixmap(*image);
-    ui->cards->setPixmap(*card);
+    //ui->cards->setPixmap(*card); adds card into ui
     this->setWindowTitle("Journey at CSULB");
 }
 
